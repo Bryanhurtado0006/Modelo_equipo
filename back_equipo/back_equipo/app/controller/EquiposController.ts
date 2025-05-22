bdb8b7efc646428b43657c044c6de8586f3d816d
Bryanhurtado0006 <bryandreshurtado18@gmail.com>
2025-05-22 16:50:33 -0500

    cammbio

---
+++ back_equipo/back_equipo/app/controller/EquiposController.ts
@@ -0,0 +1,66 @@
+import { json } from "stream/consumers";
+import pgDatabase from "../db/pgDatabase.ts";
+
+export default class EquiposController{
+    //select*from equipos
+
+    async ver_equipos({response}){
+
+
+        const resul=await pgDatabase.query('select * from equipo')
+        return response.json({MENSAJE: resul.rows})
+    }
+
+
+    async crear_equipos({request,response}){
+        const {nombre,anio_fundacion,dni_presidente}=request.body();
+        if(
+            !nombre||typeof nombre!== "string"||nombre.length === 0 ||
+            !anio_fundacion||typeof anio_fundacion!== "number" || anio_fundacion.toString().length!== 4 ||
+            !dni_presidente||isNaN(dni_presidente)
+
+        ){
+            return response.status(400).json({mensaje:"DATOS INVALIDOS BOBA"});
+        } try{
+            const resul=await pgDatabase.query(
+                `INSERT INTO public.equipo(nombre, anio_fundacion, dni_presidente) VALUES ($1,$2,$3)`,
+                 [nombre,anio_fundacion,dni_presidente]
+            );
+            if (resul.rowCount>0){
+                return response.status(201).json({ mensaje: "Equipo creado exitosamente" });
+            }else {
+            return response.status(500).json({ mensaje: "Error al crear el equipo" });
+        } 
+
+
+        }catch (error) {
+        console.error(error);
+        return response.status(500).json({ mensaje: "Error en la base de datos" });
+    }
+
+    }
+
+    async mostrar_Equipos({params}){
+        const codigo=params.codigo
+        const resul=await pgDatabase.query('select * from equipo where codigo=$1' [codigo])
+        return resul.rows[0] || {mensaje: 'equipo no encontrado'}
+    }
+
+    async actualizar_Equipo({params,request}){
+        const codigo=params.codigo
+        const {nombre,anio_fundacion,dni_presidente}=request.body()
+        await pgDatabase.query('UPDATE equipo SET codigo=$1, nombre=$2, anio_fundacion=$3, dni_presidente=$4',[nombre,anio_fundacion,dni_presidente,codigo])
+        return{ mensaje: 'equipo actualizado correctamente'}
+
+    }
+
+    async eliminar_Equipo({params}){
+        const codigo=params.codigo
+        await pgDatabase.query('DELETE FROM equipo WHERE codigo=$1',[codigo])
+        return { mensaje:'Libro eliminado' }
+    }
+
+
+
+
+}
--- back_equipo/back_equipo/start/routes/EquiposP_Router.ts
+++ back_equipo/back_equipo/start/routes/EquiposP_Router.ts
@@ -1,5 +1,6 @@
 import router from "@adonisjs/core/services/router";
 import PresiController from "../../app/controller/PresiController.js";
+import EquiposController from "../../app/controller/EquiposController.js";
 
 const Presidente=new PresiController()
 router.get('/presidente',Presidente.verPresi)
@@ -7,3 +8,14 @@ router.get('/presidente/:dni',Presidente.verPresiPorId)
 router.post('/presidente',Presidente.crearPresi)
 router.put('/presidente/:dni',Presidente.actualizarPresi)
 router.delete('/presidente/:dni',Presidente.eliminarPresi)
+
+
+// equipos
+
+const Equipos=new EquiposController()
+
+router.get('/verEquipos' , Equipos.ver_equipos);
+router.get('/MostrarEquipos/:codigo',Equipos.mostrar_Equipos);
+router.post('/crear_equipos',Equipos.crear_equipos);
+router.put('/Act_equipos',Equipos.actualizar_Equipo);
+router.delete('/delete/:codigo',Equipos.eliminar_Equipo);
