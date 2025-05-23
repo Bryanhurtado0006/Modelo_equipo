0bcfc3129515c532ec855ddba6eb5222dc59b6e5
Miguelm27 <miguem27g@gmail.com>
2025-05-22 22:44:16 -0500

    chuchaa

---
+++ front_equipos/front_equipos/src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Nav:React.FC=()=>{
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/crearPresi">Crear Presidente</Link>
+                </li>
+                <li>
+                    <Link to=""></Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+export default Nav
